d7796d6f8f936574378a13233046e87adb30b528
9oominsoo <rnasltn@ajou.ac.kr>
2020-05-24 20:34:39 +0900

    어드민 상품 페이지 생성, 패키지 상품 생성

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -2,5 +2,5 @@ class Filter < ApplicationRecord
   include ActiveModel::Serialization
 
   belongs_to :user
-  has_many :line_items, dependent: :destroy
+  # has_many :line_items, dependent: :destroy
 end
