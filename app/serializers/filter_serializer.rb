49a1f68e23dd35c12a639d679e60914493945b19
9oominsoo <rnasltn@ajou.ac.kr>
2020-05-27 23:23:38 +0900

    유저 serializer에 current_user_info 추가

--- app/serializers/filter_serializer.rb
+++ app/serializers/filter_serializer.rb
@@ -1,5 +1,5 @@
 class FilterSerializer < ActiveModel::Serializer
-  attributes %i(filter_info user_info)
+  attributes %i(filter_info user_info current_user_info)
 
   def filter_info
     { filter_id: object.id, filter_data_path: object.filter_data_path, filter_name: object.filter_name, created_at: created_date(object) }
@@ -9,6 +9,11 @@ class FilterSerializer < ActiveModel::Serializer
     user_scope = ActiveModel::Type::Boolean.new.cast(scope.dig(:params, :user_info))
     user = object.user
     { id: user.id, username: user.username, email: user.email, created_at: created_date(user) } if user_scope rescue nil
+  end
+
+  def current_user_info
+    current_user = scope.dig(:current_user)
+    { name: current_user.username, id: current_user.id}
   end
 
   def created_date target
