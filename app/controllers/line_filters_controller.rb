3e2cd5ece06f6bbbaaf9e929252c925d6690f0b3
9oominsoo <rnasltn@ajou.ac.kr>
2020-05-31 02:50:11 +0900

    체크박스 선택 시 total update

--- app/controllers/line_filters_controller.rb
+++ app/controllers/line_filters_controller.rb
@@ -1,24 +1,23 @@
 class LineFiltersController < ApiController
   before_action :authorize_request
   before_action :load_line_filter, only: %i(destroy update)
-  after_action :order_total_update, only: %i(create, destroy)
+  after_action :load_cart_order, only: %i(index, create, update)
 
   def index
-    order = @current_user.orders.cart.first
-    line_filters = order.line_filters if order.present?
+    line_filters = @order.line_filters if @order.present?
     render json: line_filters, scope: { current_user: @current_user }
   end
 
   def create
-    order = @current_user.orders.cart.first_or_create
-    line_filter = order.line_filters.create(line_filters_params)
-    order.update_total
+    line_filter = @order.line_filters.create(line_filters_params)
+    @order.update_total
     render json: line_filter
   end
 
   def update
     target = @line_filter
     (target.check?) ? target.update(check: false) : target.update(check: true)
+    @order.update_total
     render json: target
   end
 
@@ -30,6 +29,10 @@ class LineFiltersController < ApiController
   end
 
   private
+
+  def load_cart_order
+    @order = @current_user.orders.cart.first_or_create
+  end
 
   def load_line_filter
     @line_filter = LineFilter.find_by(id: params[:id])
