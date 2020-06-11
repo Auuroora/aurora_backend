d83c5a1a6fe5bafa14db187b45ead45d8d4eb968
9oominsoo <rnasltn@ajou.ac.kr>
2020-06-11 17:34:42 +0900

    fix

--- app/serializers/line_filter_serializer.rb
+++ app/serializers/line_filter_serializer.rb
@@ -12,7 +12,7 @@ class LineFilterSerializer < ActiveModel::Serializer
 
   def post_info
     post = object.post
-    { post_id: post.id, post_title: post.title }
+    { post_id: post.id, post_title: post.title } rescue nil
   end
 
   def order_info
