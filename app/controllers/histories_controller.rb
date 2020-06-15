251c2894eb71ec1ed2005000a8e705c219784a8c
9oominsoo <rnasltn@ajou.ac.kr>
2020-06-15 15:48:29 +0900

    환급 프로세스 구현

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,8 +1,21 @@
 class HistoriesController < ApiController
   before_action :authorize_request
+  before_action :load_history, only: %i(update)
 
   def index
     histories = @current_user.histories
     render json: histories
+  end
+
+  def update
+    if (state = params[:state])
+      (state.eql?("exchange_request")) ? @history.exchange_request! : @history.returned_as_cash!
+    end
+  end
+
+  private
+
+  def load_history
+    @history = History.find_by(id: params[:id])
   end
 end
--- app/models/history.rb
+++ app/models/history.rb
@@ -7,15 +7,30 @@ class History < ApplicationRecord
   aasm.attribute_name :state
   aasm do
     state :non_exchange, initial: true
+    state :cashed
     state :exchange_requested
     state :exchanged
+
+    after_all_transitions :log_state_change
+
+    event :returned_as_cash do
+      transitions from: [:non_exchange], to: :cashed, after: proc { self.exchanged_at = DateTime.now }
+    end
 
     event :exchange_request do
       transitions from: [:non_exchange], to: :exchange_requested
     end
 
     event :exchange do
-      transitions from: [:exchange_requested], to: :exchanged,  after: proc { self.exchanged_at = DateTime.now }
+      transitions from: [:exchange_requested], to: :exchanged, after: proc { self.exchanged_at = DateTime.now }
+    end
+  end
+
+  def log_state_change
+    if (aasm.to_state == :cashed)
+      seller = self.filter.user
+      seller.cash += self.amount
+      seller.save!
     end
   end
 end
