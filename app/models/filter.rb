d8b3e2900f12a2b437f491d09de7b7bc6960775e
9oominsoo <rnasltn@ajou.ac.kr>
2020-06-17 02:43:09 +0900

    필터 삭제시 해당 게시글도 함께 삭제 처리

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -3,5 +3,7 @@ class Filter < ApplicationRecord
 
   belongs_to :user
   has_many :histories, dependent: :destroy
+  has_many :posts, dependent: :destroy
+
   # has_many :line_items, dependent: :destroy
 end
