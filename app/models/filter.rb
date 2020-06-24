271cf8d03e6027185f075cc4da67266b6daf0b76
9oominsoo <rnasltn@ajou.ac.kr>
2020-06-25 02:39:10 +0900

    filter cascades 추가

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -5,6 +5,7 @@ class Filter < ApplicationRecord
   belongs_to :user
   has_many :histories, dependent: :destroy
   has_many :posts, dependent: :destroy
+  has_many :line_items, dependent: :destroy
 
   # has_many :line_items, dependent: :destroy
 end
